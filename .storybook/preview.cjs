5a10d58249f94bc5936f935682a0632a99bd5010
Joao Teles <joaojoseteles@gmail.com>
2023-01-22 19:10:35 -0300

    :recycle: refactor: configuraçao do msw no storybook

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,5 +1,12 @@
 import { themes } from "@storybook/theming";
 import "../src/styles/global.css";
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+initialize({
+  onUnhandledRequest: "bypass",
+});
+
+export const decorator = [mswDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
