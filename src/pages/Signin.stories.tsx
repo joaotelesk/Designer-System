89c415ec332ebaa4ca7912cec417015b8559ad1b
Joao Teles <joaojoseteles@gmail.com>
2023-01-22 19:11:52 -0300

    :recycle: refactor: utilizando axios e msw para simular conexao com api

--- src/pages/Signin.stories.tsx
+++ src/pages/Signin.stories.tsx
@@ -1,23 +1,38 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { within, userEvent, waitFor } from "@storybook/testing-library";
 import { expect } from "@storybook/jest";
+import { rest } from "msw";
 import { SignIn } from "./Signin";
 
 export default {
-  titulo: "Pages/Sing in",
+  title: "Pages/Sign in",
   component: SignIn,
   args: {},
   argTypes: {},
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post("/sessions", (req, res, ctx) => {
+          return res(
+            ctx.json({
+              message: "Login realizado!",
+            })
+          );
+        }),
+      ],
+    },
+  },
 } as Meta;
 
 export const Default: StoryObj = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
+
     userEvent.type(
       canvas.getByPlaceholderText("Digite seu e-mail"),
-      "joaojoseteles@gmail.com"
+      "diego@rocketseat.com.br"
     );
-    userEvent.type(canvas.getByPlaceholderText("*******"), "123456");
+    userEvent.type(canvas.getByPlaceholderText("******"), "12345678");
 
     userEvent.click(canvas.getByRole("button"));
 
